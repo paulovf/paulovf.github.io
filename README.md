43898bd0d95a33126aa5678cc8d1e5cadd338505
paulovf <paulovfrancisco@gmail.com>
2016-12-14 17:24:42 -0200

    criado: arquivos do projeto do site pessoal

---
+++ README.md
@@ -0,0 +1,6 @@
+# site-paulovitor
+Repositório com o site pessoal de Paulo Vitor
+
+Desenvolvido por: Paulo Vitor Francisco
+
+Paulo Viotr - 2016 - Todos os Direitos Reservados
---
+++ js/script_site.js
@@ -0,0 +1,35 @@
+/*
+ *
+ * Rádio Legião - 2016
+ *
+ * Desenvolvido por: 
+ * Paulo Vitor Francisco
+ * 
+ * Todos os direitos reservados.
+ *
+*/
+
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2015 The Bootstrap Authors
+ * Copyright 2014-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+// See the Getting Started docs for more information:
+// https://getbootstrap.com/getting-started/#support-ie10-width
+
+(function () {
+  'use strict';
+
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.head.appendChild(msViewportStyle)
+  }
+
+})();
